c802dc551187c7686e3d810c93693fb17a7666d1
aminul <rahathssn@gmail.com>
2025-03-14 15:38:19 +0600

    added user model

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,20 +1,20 @@
 import { Request, Response } from "express";
-import { userService } from "./user.service";
 import catchAsync from "../../../shared/catchAsync";
 import httpStatus from "http-status";
 import sendResponse from "../../../shared/sendResponse";
+import { UserService } from "./user.service";
 
 // request and response are handled by controller
 const createAdmin = catchAsync(async (req: Request, res: Response) => {
-  const result = await userService.createAdmin();
+  const result = await UserService.createAdmin(req);
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: "Admin Created successfully!",
+    message: "User Created successfully!",
     data: result,
   });
 });
 
-export const userController = {
+export const UserController = {
   createAdmin,
 };
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,6 +1,44 @@
-const createAdmin = () => {
-  console.log("request has been sent from controller");
+import { Request } from "express";
+import prisma from "../../../shared/prisma";
+import ApiError from "../../errors/ApiErrors";
+import httpStatus from "http-status";
+import { generate } from "generate-password";
+import * as bcrypt from "bcrypt";
+import config from "../../../config";
+
+const createAdmin = async (req: Request) => {
+  console.log(req.body);
+
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email: req.body.email,
+    },
+  });
+
+  if (existingUser) {
+    throw new ApiError(httpStatus.CONFLICT, "User already exists");
+  }
+
+  const generatePassword: string = generate({
+    length: 10,
+    numbers: true,
+  });
+
+  const hashedPassword: string = await bcrypt.hash(
+    generatePassword,
+    Number(config.bcrypt_salt_rounds)
+  );
+
+  console.log("Generated password", generatePassword);
+  console.log("hashedPassword", hashedPassword);
+
+  const userData = {
+    email: req.body.email,
+    password: hashedPassword,
+    role: req.body.role,
+  };
 };
-export const userService = {
+
+export const UserService = {
   createAdmin,
 };
